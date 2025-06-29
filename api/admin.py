f54034e6a904c3c4089fa31077f9c675752769fb
Hazemzaky <120676558+Hazemzaky@users.noreply.github.com>
2025-06-29 16:05:20 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Account, JournalEntry, Transaction
 
-# This line tells Django to show the Account model in the admin site
-# We can add some customizations later if we want.
-admin.site.register(Account)
+# This new class tells Django how to safely handle the Account model
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('account_number', 'name', 'account_type', 'parent')
+    list_filter = ('account_type',)
+    search_fields = ('name', 'account_number')
+    # This is the line that prevents the infinite loop recursion
+    raw_id_fields = ('parent',)
 
-# We can also register the other models to view them later
+# We register the Account model using our new custom class
+admin.site.register(Account, AccountAdmin)
+
+# The other models are still registered simply
 admin.site.register(JournalEntry)
 admin.site.register(Transaction)
