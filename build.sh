af19716fbd96cda2488b4bcf474b0ec809ad215c
Hazemzaky <120676558+Hazemzaky@users.noreply.github.com>
2025-06-29 09:32:23 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# STEP 1: Create the migration files for our 'api' app
+python manage.py makemigrations api
+
+# STEP 2: Apply those migrations to build the tables in the database
 python manage.py migrate
+
+# STEP 3: Collect static files
+python manage.py collectstatic --no-input
