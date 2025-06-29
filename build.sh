b9b4e3da3b151bbcfce147a3ac85b565550b0fdf
Hazemzaky <120676558+Hazemzaky@users.noreply.github.com>
2025-06-29 11:20:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,13 +5,5 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Create and apply database migrations
-python manage.py makemigrations api
-python manage.py migrate
-
-# Create a superuser using environment variables (if they exist)
-# This is the official, non-interactive method.
-python manage.py createsuperuser --noinput || echo "Superuser already exists, skipping."
-
 # Collect static files
 python manage.py collectstatic --no-input
