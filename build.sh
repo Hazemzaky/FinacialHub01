0eab78d342344575ab2158a657b6e0431172f85d
Hazemzaky <120676558+Hazemzaky@users.noreply.github.com>
2025-06-29 15:38:04 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files for production
-python manage.py collectstatic --no-input
